3210d7f592f2d4c405a203fd714af1f519f24567
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2015-12-13 19:19:12 -0430

    Actualizando buenavista.sql
    Incluyendo tabla customers con sus campos correspondientes.

--- buenavista.sql
+++ buenavista.sql
@@ -10,7 +10,16 @@ modified DATETIME DEFAULT NULL
 CREATE TABLE customers (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(50),
-lastname VARCHAR(255),
+lastname VARCHAR(50),
+identification VARCHAR(13),
+package VARCHAR(255),
+price VARCHAR(255),
+device VARCHAR(255),
+registration_date DATETIME DEFAULT NULL,
+cell_phone VARCHAR(255),
+address VARCHAR(255),
+mac_address VARCHAR(255),
+observation VARCHAR(255),
 created DATETIME DEFAULT NULL,
 modified DATETIME DEFAULT NULL
 );
