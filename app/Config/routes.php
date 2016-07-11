3e5359cc80664d9648b44c407f099e757844b1f8
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2016-07-11 17:54:34 -0400

    Apuntando al controlador customers pagina index

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,11 +25,12 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'customers', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	/**Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));*/
+	Router::connect('/pages/*', array('controller' => 'customers', 'action' => 'index'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
